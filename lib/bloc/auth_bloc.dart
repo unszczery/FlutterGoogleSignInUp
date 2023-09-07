130e7c51443e856edaaeeddc9d659b5675957e43
ThouSelf <sasatokoi@gmail.com>
2023-09-07 20:21:59 +0300

    minor

--- lib/bloc/auth_bloc.dart
+++ lib/bloc/auth_bloc.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'package:bloc/bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:news_getter/repository/auth_repository.dart';
